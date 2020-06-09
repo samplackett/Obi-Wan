26f9274d5881732067b57a9b4dab7a22c800ebf0
samleplack <sam.plackett@gmail.com>
2020-06-09 12:10:25 +0100

    Updates

---
+++ precache-manifest.abd6a9664697f70bb754cd99b0f84357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8421b09b48dde8d63bdaa1877d66329",
+    "url": "/Obi-Wan/index.html"
+  },
+  {
+    "revision": "b553443f9d16cb543172",
+    "url": "/Obi-Wan/static/css/main.f13d6864.chunk.css"
+  },
+  {
+    "revision": "e590e120a3c909c1fcc0",
+    "url": "/Obi-Wan/static/js/2.9cc8f590.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Obi-Wan/static/js/2.9cc8f590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b553443f9d16cb543172",
+    "url": "/Obi-Wan/static/js/main.2c3b5c9f.chunk.js"
+  },
+  {
+    "revision": "c4e97a25f33c26689565",
+    "url": "/Obi-Wan/static/js/runtime-main.fff4143d.js"
+  }
+]);
